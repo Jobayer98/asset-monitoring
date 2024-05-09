67b18aeb419cc0e405e3a3a7b5c7c352614cf4d3
jobayer <jobayerrahman0220@gmail.com>
2024-05-09 16:13:15 +0600

    add device and devicelog model

--- asset_monitoring_api/models.py
+++ asset_monitoring_api/models.py
@@ -19,3 +19,19 @@ class Employee(models.Model):
     
     def __str__(self) -> str:
         return f'{self.user.first_name} {self.user.last_name}'
+    
+
+class Device(models.Model):
+    device_name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    current_employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.device_name
+    
+class DeviceLog(models.Model):
+    checked_out_time = models.DateField()
+    returned_time = models.DateTimeField(null=True, blank=True)
+    condition_when_handed_out = models.TextField()
+    condition_when_returned = models.TextField(null=True, blank=True)
+    device = models.ManyToManyField(Device)
