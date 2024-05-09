d9dcdc198dddebd1536be34bfdf301458e2c2519
jobayer <jobayerrahman0220@gmail.com>
2024-05-09 16:12:12 +0600

    add company and employee model

--- asset_monitoring_api/models.py
+++ asset_monitoring_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    designation = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f'{self.user.first_name} {self.user.last_name}'
